41d7fbedd691e75d89e9dfdcf9a130f1637dd387
Gordon Heydon <gordon@heydon.com.au>
2009-12-14 11:05:30 +0000

    * Clean up vertical tabs on node types page

--- simple_access.js
+++ simple_access.js
@@ -2,7 +2,16 @@ Drupal.verticalTabs = Drupal.verticalTabs || {};
 
 Drupal.verticalTabs.sa = function() {
   if (!$('.vertical-tabs-sa .form-checkbox:checked').size()) {
-    return Drupal.t('Public access');
+    return Drupal.t('No access set');
+  }
+  else {
+    return Drupal.t('Restricted access');
+  }
+}
+
+Drupal.verticalTabs.simple_access = function() {
+  if (!$('.vertical-tabs-simple_access .form-checkbox:checked').size()) {
+    return Drupal.t('No access set');
   }
   else {
     return Drupal.t('Restricted access');
